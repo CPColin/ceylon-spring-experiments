beac2a04cec01d59dc56982f4f01c459be7c83aa
Colin Bartolome <cbartolo@crappycomic.com>
2018-04-09 19:44:13 -0700

    Partially implemented fragment inclusion script

--- resource/demo/ROOT/templates/Layout.js
+++ resource/demo/ROOT/templates/Layout.js
@@ -1,34 +1,109 @@
 /*
- * Utility script that performs the Layout fragment substitution in static mode, to keep template natural.
+ * Utility script that performs fragment substitutions in static mode, to keep templates natural.
  * 
- * Works by default in Firefox, but Chrome needs the --allow-file-access-from-files command-line argument.
+ * Works by default in Firefox, but Chrome needs the --allow-file-access-from-files command-line
+ * argument or it'll block the request for security reasons.
  */
 
-var html = document.getElementsByTagName("html")[0];
-var body = document.getElementsByTagName("body")[0].cloneNode(true);
-var styles = document.getElementsByTagName("style");
-var style = styles.length ? styles[0].cloneNode(true) : null;
-var title = document.title;
-var request = new XMLHttpRequest();
+function doRequest(uri) {
+    var request = new XMLHttpRequest();
 
-request.open("GET", "fragments/Layout.html", false);
-request.send();
+    request.open("GET", uri, false);
+    request.send();
 
-html.innerHTML = request.responseText;
-
-var content = document.getElementById("content");
-
-while (body.childNodes.length > 0)
-{
-   content.appendChild(body.childNodes[0]);
+    return request.responseText;
 }
 
-if (style)
-{
-   var head = html.getElementsByTagName("head")[0];
-   
-   head.appendChild(style);
+function doLayout() {
+    var html = document.getElementsByTagName("html")[0];
+    var body = document.getElementsByTagName("body")[0].cloneNode(true);
+    var styles = document.getElementsByTagName("style");
+    var style = styles.length ? styles[0].cloneNode(true) : null;
+    var title = document.title;
+
+    html.innerHTML = doRequest("fragments/Layout.html");
+
+    var content = document.getElementById("content");
+
+    while (body.childNodes.length > 0)
+    {
+        content.appendChild(body.childNodes[0]);
+    }
+
+    if (style)
+    {
+        var head = html.getElementsByTagName("head")[0];
+        
+        head.appendChild(style);
+    }
+
+    document.title = title;
 }
 
-document.title = title;
+function getFragmentReplacements() {
+    return document.querySelectorAll("html *[data-th-replace^=fragments]");
+}
+
+function doFragments() {
+    var elements = getFragmentReplacements();
+    
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var fragmentExpression = element.attributes["data-th-replace"].value;
+        var tokens = fragmentExpression.split("::");
+        var uri = tokens[0].trim();
+        var fragment = tokens[1].trim();
+        
+        if (uri == "fragments/formControls.html") {
+            doFormControls(element, uri, fragment);
+        }
+    }
+}
+
+function getFragmentName(fragment) {
+    return fragment.split("(")[0];
+}
+
+function getFragmentArguments(fragment) {
+    var argumentList = fragment.substring(fragment.indexOf("(") + 1, fragment.lastIndexOf(")"));
+    var arguments = argumentList.split(",");
+    
+    for (var i = 0; i < arguments.length; i++) {
+        var argument = arguments[i].trim();
+        
+        if (argument.startsWith("'") && argument.endsWith("'")) {
+            argument = argument.substring(1, argument.length - 1);
+        }
+        
+        arguments[i] = argument;
+    }
+    
+    return arguments;
+}
+
+function doFormControls(destination, uri, fragment) {
+    var fragmentName = getFragmentName(fragment);
+    var fragmentArguments = getFragmentArguments(fragment);
+    var html = document.createElement("html");
+    
+    html.innerHTML = doRequest(uri);
+        
+    var element = html.querySelector("[data-th-fragment^=" + fragmentName + "]");
+
+    if (fragmentName == "text") {
+        var label = element.querySelector("label");
+        var input = element.querySelector("input");
+        
+        label.innerHTML = fragmentArguments[0] + ":";
+        label.htmlFor = fragmentArguments[1];
+        input.id = fragmentArguments[1];
+        input.name = fragmentArguments[1];
+    }
+    
+    destination.parentNode.replaceChild(element, destination);
+}
+
+doLayout();
+
+doFragments();
 
